5a8e9022b25da2901e7e675c6707ee67c1e891b2
yurysilva74 <yurysilva74@gmail.com>
2021-03-12 22:37:26 -0300

    novo arquivo python para calcular área do triângulo

---
+++ aulasFiap/calcTriang.py
@@ -0,0 +1,9 @@
+#cálculo da área de um triâgulo
+aux = input("Base: ")
+base = float(aux)
+
+aux = input("Altura: ")
+altura = float(aux)
+
+area = base * altura / 2
+print("A area vale ", area, "metros quadrados")
