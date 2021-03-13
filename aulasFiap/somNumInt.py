9dda39588bcd54adb36f7ece1edc229d7af1a820
yurysilva74 <yurysilva74@gmail.com>
2021-03-12 22:21:38 -0300

    Prieiros passo em pythom na faculdade.

---
+++ aulasFiap/somNumInt.py
@@ -0,0 +1,10 @@
+valor = input("digite um número: ")
+numA = int(valor)
+
+valor = input("Digite um número: ")
+numB = int(valor)
+
+valor = input("Digite um número: ")
+numC = int(valor)
+
+print(numA + numB + numC)
