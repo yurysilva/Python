865974d75552c48316799be66dcbeab9fa00b5fd
yurysilva74 <yurysilva74@gmail.com>
2021-03-28 17:55:49 -0300

    Alterado comentario com erro de gramatica.

--- atividades/horas_professor.py
+++ atividades/horas_professor.py
@@ -2,7 +2,7 @@
 aulas_semanais = int(input("Digite a quantidade de aulas por semana: "))
 hora_aula = float(input("Digite o valor da aula por hora: "))
 
-#Seão feitos os calculos necessários para saber o valor total do salário mensal do professor
+#Serão feitos os calculos necessários para saber o valor total do salário mensal do professor
 salario_base = aulas_semanais * 4.5 * hora_aula
 hora_atividade = salario_base * 0.05
 dsr = ((salario_base + hora_atividade)/6)
