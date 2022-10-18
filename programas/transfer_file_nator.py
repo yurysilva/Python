cf728041a99519ec13e317258129eddb9fe327c6
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-10-18 09:24:58 -0300

    comentando o código para ter um entedimento mais claro do programa.

--- programas/transfer_file_nator.py
+++ programas/transfer_file_nator.py
@@ -4,6 +4,7 @@ import re
 import datetime
 import sys
 
+#Recebe argumentos e valida se precisa de 3 ou de apenas 2
 try:
     argv = sys.argv
     args_data_completa = argv[2]
@@ -14,10 +15,7 @@ except:
     args_data_completa = argv[2]
     args_caminho = argv[1]
 
-caminho = args_caminho.replace('\\','/')
-
-enconding_ = 'utf-8'
-
+#busca informações do arquivo .json
 with open(args_caminho, 'r', encoding='utf-8') as itf:
     json_objeto = json.load(itf)
     url = json_objeto['url']
@@ -25,7 +23,7 @@ with open(args_caminho, 'r', encoding='utf-8') as itf:
     nome = json_objeto['nome_arquivo']
 
 
-
+#valida a quantidade de dadtas que vai trabalhar na url
 if (len(comparcao := re.findall("\*dt", url)) > 0) and (len(comparcao := re.findall("\*dt", url)) < 2):
     url2 = re.sub("\*dt",args_data_completa, url)
     name2 = re.sub("\*dt", args_data_completa, nome)
@@ -34,14 +32,12 @@ if (len(comparcao := re.findall("\*dt", url)) > 0) and (len(comparcao := re.find
     with open(destino+name2, 'wb') as f:
         f.write(r.content)
 
-teste = '07'
 if len(comparcao := re.findall("\*dt", url)) > 1:
     url2 = url.replace('*dty',args_data_completa)
     url3 = url2.replace('*dtm',args_mes)
 
     name2 = re.sub("\*dty",args_data_completa, nome)
     name3 = re.sub("\*dtm", args_mes, name2)
-    #url_arquivo = url2
     r = requests.get(url3)
     with open(destino+name3, 'wb') as f:
         f.write(r.content) 
