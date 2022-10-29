6ea6cf99ad2fa4f8b7e1782884015760709c27f1
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-10-28 23:41:09 -0300

    Adicinando informações nos templates de index e contato.

--- django/comeco_com_django/core/views.py
+++ django/comeco_com_django/core/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'curso': 'Programação Web com Django Framework',
+        'jennifer': 'Eu amo a minha namorada!'
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request,'contato.html')
