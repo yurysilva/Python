8dc6b309bf970749aeeac1ebb4e11a65d5a6b608
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-11-02 22:29:50 -0300

    Criando nova migration de Cliente

--- django/comeco_com_django/core/models.py
+++ django/comeco_com_django/core/models.py
@@ -5,3 +5,8 @@ class Produto(models.Model):
     nome = models.CharField('nome', max_length=100)
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em Estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.CharField('E-mail', max_length=100)
