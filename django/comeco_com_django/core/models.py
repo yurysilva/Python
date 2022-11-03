318b50fb68b826381402b3b0f5af8008fd07d354
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-11-02 22:11:48 -0300

    Adicionando models e migrations

--- django/comeco_com_django/core/models.py
+++ django/comeco_com_django/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+    estoque = models.IntegerField('Quantidade em Estoque')
