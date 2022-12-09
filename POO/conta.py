4dd06142cd4607861bce2ca86febb8360f09f8af
Yury Silva - BDS DataSolution <ysilva@bdsdatasolution.com.br>
2022-12-09 16:42:34 -0300

    Aidicionando metodos a classe e implementando eles nos meus metodos com o file de teste.

--- POO/conta.py
+++ POO/conta.py
@@ -3,19 +3,23 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f'O seu saldo atual é de {self.saldo} reais')
+        print(f'O seu saldo atual é de {self.__saldo} reais')
 
     def deposito(self, depositar):
-        self.saldo += depositar
-        if self.saldo > self.limite:
+        self.__saldo += depositar
+        if self.__saldo > self.__limite:
             print("Você ultrapassou o limite, por favor coloque um valor menor")
-            self.saldo -= depositar
+            self.__saldo -= depositar
     
     def saque(self, sacar):
-        self.saldo -= sacar
+        self.__saldo -= sacar
+
+    def transferir(self, valor, destino):
+        self.saque(valor)
+        destino.deposito(valor)
